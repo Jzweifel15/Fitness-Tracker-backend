7a93b4bf98976a91b742ade5e6ada5ac0348e510
Jzweifel15 <jzweifel15@yahoo.com>
2021-02-21 12:52:56 -0500

    Meals API
    API for all meals in the database

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,14 @@
 class MealsController < ApplicationController
+
+  def index
+    meals = Meal.all 
+    render json: meals 
+  end
+
+  private 
+
+  def meal_params
+    params.require(:meal).permit(:name, :numServings, :calories)
+  end
+
 end
