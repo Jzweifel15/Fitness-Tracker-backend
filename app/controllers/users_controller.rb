bc09fa0d14a64f863836efc52e3ebdc411ea2790
Jzweifel15 <jzweifel15@yahoo.com>
2021-02-21 12:48:40 -0500

    Users API
    Basic API for displaying all users in the database and an API for rendering a specific user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all 
+    render json: users 
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: UsersSerializer.new(user)
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :weight, :height, :gender, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:index, :new, :create]
-  resources :meals, only: [:index, :new, :create]
-  resources :exercises, only: [:index, :new, :create]
+  resources :users, only: [:index, :show, :new]
+  resources :meals, only: [:index, :new]
+  resources :exercises, only: [:index, :new]
 
 end
