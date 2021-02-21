13da29755136fef404dfc107f4120a78722af9de
Jzweifel15 <jzweifel15@yahoo.com>
2021-02-21 12:55:09 -0500

    Exercises API
    API that renders all exercises in the database to possibly be used to pull data from for the front-end. Possible changes could be made based on certain outcomes

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,14 @@
 class ExercisesController < ApplicationController
+
+  def index
+    exercises = Exercise.all 
+    render json: exercises 
+  end
+
+  private 
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :numSets, :numReps)
+  end
+
 end
