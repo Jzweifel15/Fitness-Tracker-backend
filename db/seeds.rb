4655138c2b48e2edf722671ff56c0721272374f8
Jzweifel15 <jzweifel15@yahoo.com>
2021-02-21 12:19:39 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create!(name: "Justin Zweifel", weight: 220, height: 74, gender: "Male", email: "justin.z@email.com", password: "12345")
+
+exercise1 = Exercise.create!(name: "Bench Press", numSets: 5, numReps: 10, user_id: user1.id)
+exercise2 = Exercise.create!(name: "Dumbell Flys", numSets: 5, numReps: 15, user_id: user1.id)
+
+meal1 = Meal.create!(name: "Chicken Alfredo", numServings: 2, calories: 450, user_id: user1.id)
+meal2 = Meal.create!(name: "Steak and Potatos", numServings: 1, calories: 400, user_id: user1.id)
+
