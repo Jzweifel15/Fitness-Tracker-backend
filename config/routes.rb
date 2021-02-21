b52636307570cb90e5d35b4956991d2b51ab39d7
Jzweifel15 <jzweifel15@yahoo.com>
2021-02-21 12:56:28 -0500

    Update routes.rb
    Updated the routes to allow for all possible RESTful routes based on the possible scenarios in the front-end. Possible changes could be made based on certain outcomes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:index, :show, :new]
-  resources :meals, only: [:index, :new]
-  resources :exercises, only: [:index, :new]
+  resources :users
+  resources :meals
+  resources :exercises
 
 end
