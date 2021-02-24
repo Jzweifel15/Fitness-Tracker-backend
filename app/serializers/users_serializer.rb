23339936e4b646a810133abd3766073c63359903
Jzweifel15 <jzweifel15@yahoo.com>
2021-02-24 00:05:51 -0500

    Update users_serializer.rb

--- app/serializers/users_serializer.rb
+++ app/serializers/users_serializer.rb
@@ -1,6 +1,4 @@
 class UsersSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :weight, :height, :gender, :email
-  has_many :exercises
-  has_many :meals
+  attributes :name, :weight, :height, :gender, :email, :exercises, :meals
 end
