6bd6e5ec9e754bbfb158f3c1b6328d677e25731a
Jzweifel15 <jzweifel15@yahoo.com>
2021-03-02 06:07:58 -0500

    Destroy Meals
    destroys a meal record from the database

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -5,9 +5,19 @@ class MealsController < ApplicationController
     render json: meals 
   end
 
+  def show
+    meal = Meal.find(params[:id])
+    render json: meal
+  end
+
   def create
     meal = Meal.create(meal_params)
     render json: meal
+  end
+
+  def destroy
+    meal = Meal.find(params[:id])
+    meal.destroy
   end
 
   private 
