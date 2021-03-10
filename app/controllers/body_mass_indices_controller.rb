0fdc0ee394dad6fd9c4bc46072b76d984bc4463e
Jzweifel15 <jzweifel15@yahoo.com>
2021-03-10 07:32:18 -0500

    Create New BMI
    creates a new bmi record to the database when the user saves a new bmi calculation

--- app/controllers/body_mass_indices_controller.rb
+++ app/controllers/body_mass_indices_controller.rb
@@ -1,15 +1,24 @@
 class BodyMassIndicesController < ApplicationController
 
-  def create
+  def index 
+    bmis = BodyMassIndex.all 
+    render json: bmis 
   end
 
-  def destroy
+  def show
+    bmi = BodyMassIndex.find(params[:id])
+    render json: bmi
+  end
+
+  def create
+    bmi = BodyMassIndex.create(body_mass_index_params)
+    render json: bmi
   end
 
   private
 
   def body_mass_index_params
-    params.require(:body_mass_index).permit(:weight, :height, :user_id)
+    params.require(:body_mass_index).permit(:weight, :height, :bmi, :user_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users
   resources :meals
   resources :exercises
+  resources :body_mass_indices
 
 end
